27b47f06fcb1d476e816e36daa87135601a96425
Siriuns-X <Siriuns@icloud.com>
2026-09-16 20:10:22 +0800

    chore: add nix dev environment

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "satteri-mathml dev environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAll = f: nixpkgs.lib.genAttrs systems (s: f nixpkgs.legacyPackages.${s});
+    in
+    {
+      devShells = forAll (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.nodejs_24
+            pkgs.pnpm_11
+          ];
+        };
+      });
+    };
+}
